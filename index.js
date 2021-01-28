9bddecc087dde9f90b7e2b048c2401be67775800
mihailtomov <62585647+mihailtomov@users.noreply.github.com>
2021-01-28 12:14:53 +0200

    Added view engine and transferred necessary resources
    Configured handlebars view engine and transferred template, images and css files to project directory

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const config = require('./config/config-port');
 
 const app = express();
 
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
+
+app.get('/', (req, res) => {
+    res.render('home', { layout: false });
+});
